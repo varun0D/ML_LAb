3b97229e8e3f4f833641be14e2fa5db0d3e07804
VARUN D <varun030406@gmail.com>
2026-07-28 08:47:14 +0530

    Refactor 3D plot creation for Iris dataset

--- 2a.py
+++ 2a.py
@@ -2,25 +2,20 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn.datasets import load_iris
 
-# Load Iris dataset
 iris = load_iris()
 data = pd.DataFrame(iris.data, columns=iris.feature_names)
 
-# Select columns
-x = data['sepal length (cm)']
-y = data['sepal width (cm)']
-z = data['petal length (cm)']
+ax = plt.axes(projection='3d')
 
-# Create 3D Surface Plot
-fig = plt.figure()
-ax = fig.add_subplot(111, projection='3d')
+ax.plot_trisurf(
+    data['sepal length (cm)'],
+    data['sepal width (cm)'],
+    data['petal length (cm)'],
+    cmap='viridis'
+)
 
-ax.plot_trisurf(x, y, z, cmap='viridis')
-
-# Labels
 ax.set_xlabel('Sepal Length')
 ax.set_ylabel('Sepal Width')
 ax.set_zlabel('Petal Length')
-ax.set_title('3D Surface Plot of Iris Dataset')
 
 plt.show()
